fb1cb6f8963988f84f15ab9d228194e55cb41f59
Samie Azubike <85092681+samieteq@users.noreply.github.com>
2023-08-01 11:56:10 +0100

    Updated

--- registrationsystem-ui/src/app/layout.tsx
+++ registrationsystem-ui/src/app/layout.tsx
@@ -19,12 +19,14 @@ export default function RootLayout({
   return (
 		<html lang="en">
 			<body className={inter.className}>
+			{/*
 				<div className="w-full py-2 text-white absolute bg-indigo-600 top-0 left-0 text-center text-[12px]">
 					<p>
 						Notice: I&apos;m trying to deploy the backend to AWS, so
 						it may not be working properly with the frontend now, but it working locally
 					</p>
 				</div>
+			*/}
 				<NextAuthProvider>
 					<Toaster />
 					{children}
