016e6c4a3fa0c88410bbb457fc787e126ab70613
Samie Azubike <85092681+samieteq@users.noreply.github.com>
2023-08-01 17:43:09 +0100

    updated

--- registrationsystem-ui/src/app/constant.ts
+++ registrationsystem-ui/src/app/constant.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL = "https://ec2-18-207-162-145.compute-1.amazonaws.com:443/api/v1/";
+export const SERVER_URL = "https://ec2-54-162-105-182.compute-1.amazonaws.com:443/api/v1/";
